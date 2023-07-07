ee60845941aeb70ff13c94ea9496911faea2146c
Michał <michauz2001@gmail.com>
2023-07-07 15:03:58 +0200

    Configure Application config to accept all requests (not safe yet, need to add JWT token authorization) and Web config to enable swagger-ui site at http://server:port/swagger-ui/index.html

---
+++ src/main/kotlin/pl/poznan/put/decision_support/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package pl.poznan.put.decision_support.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("pl.poznan.put")
+open class WebConfig: WebMvcConfigurer {
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/")
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/")
+    }
+
+}
