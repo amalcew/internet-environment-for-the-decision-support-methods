302f43f77606d7c0446e7458c15770b454f137a6
Alexander Malcew <amalcew@protonmail.com>
2023-07-19 19:00:25 +0200

    tests aggregation added

--- src/main/kotlin/pl/poznan/put/decision_support/sample/controller/Electre1sController.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/sample/controller/Electre1sController.kt
@@ -3,20 +3,20 @@ package pl.poznan.put.decision_support.sample.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import pl.poznan.put.decision_support.sample.service.electre1s.Criterion
+import pl.poznan.put.decision_support.sample.service.electre1s.Electre1sCalculator
 import pl.poznan.put.decision_support.sample.service.electre1s.Variant
-import pl.poznan.put.decision_support.sample.service.electre1s.steps.ConcordanceTest
-import pl.poznan.put.decision_support.sample.service.electre1s.steps.DiscordanceTest
 import java.util.LinkedList
 
 @RestController
 class Electre1sController() {
 
+    val electre1s = Electre1sCalculator()
     @GetMapping("/electre1s")
-    fun getAllUsers(): Array<Array<Double>> {
-        var calculator = DiscordanceTest()
-        var kryteria = LinkedList<Criterion>()
+    fun run(): Array<Array<Double>> {
+        val lambda: Double = 0.6
+        val kryteria = LinkedList<Criterion>()
 
-        var kryterium1 = Criterion()
+        val kryterium1 = Criterion()
         kryterium1.q = 0.9
         kryterium1.p = 2.2
         kryterium1.v = 3.0
@@ -24,7 +24,7 @@ class Electre1sController() {
         kryterium1.preferenceType = kryterium1.PREFERENCE_TYPE_COST
         kryteria.add(kryterium1)
 
-        var kryterium2 = Criterion()
+        val kryterium2 = Criterion()
         kryterium2.q = 1.0
         kryterium2.p = 1.6
         kryterium2.v = 3.5
@@ -32,22 +32,20 @@ class Electre1sController() {
         kryterium2.preferenceType = kryterium2.PREFERENCE_TYPE_GAIN
         kryteria.add(kryterium2)
 
-        var wariant1 = Variant()
+        val wariant1 = Variant()
         wariant1.values = arrayOf(10.8, 4.7)
 
-        var wariant2 = Variant()
+        val wariant2 = Variant()
         wariant2.values = arrayOf(8.0, 6.0)
 
-        var wariant3 = Variant()
+        val wariant3 = Variant()
         wariant3.values = arrayOf(11.0, 4.8)
 
-        var warianty = Array(3) { Variant() }
+        val warianty = Array(3) { Variant() }
         warianty[0] = wariant1
         warianty[1] = wariant2
         warianty[2] = wariant3
 
-        val res = calculator.calculate(warianty, kryteria)
-
-        return res
+        return electre1s.calculate(warianty, kryteria, lambda)
     }
 }
--- src/main/kotlin/pl/poznan/put/decision_support/sample/service/electre1s/Electre1sCalculator.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/sample/service/electre1s/Electre1sCalculator.kt
@@ -1,7 +1,36 @@
 package pl.poznan.put.decision_support.sample.service.electre1s
 
+import pl.poznan.put.decision_support.sample.service.electre1s.steps.ConcordanceTest
+import pl.poznan.put.decision_support.sample.service.electre1s.steps.DiscordanceTest
+
+import java.util.*
+
 class Electre1sCalculator {
-    public fun calculate(variants: Array<Variant>, criteria: Array<Criterion>) {
-    // TODO
+    public fun calculate(variants: Array<Variant>, criteria: LinkedList<Criterion>, lambda: Double): Array<Array<Double>> {
+        val concordanceTest = ConcordanceTest()
+        val discordanceTest = DiscordanceTest()
+
+        val cTestResults = concordanceTest.calculate(variants, criteria)
+        val dTestResults = discordanceTest.calculate(variants, criteria)
+        val res = this.aggregateTests(cTestResults, dTestResults, lambda)
+
+        return res
+    }
+
+    private fun aggregateTests(concordanceTestResults: Array<Array<Double>>, discordanceTestResults: Array<Array<Double>>, lambda: Double): Array<Array<Double>> {
+        val n: Int = concordanceTestResults.size
+        val result: Array<Array<Double>> = Array(n) { Array(n) { 0.0 } }
+
+        for (row in concordanceTestResults.indices) {
+            for (column in 0 until concordanceTestResults[row].size) {
+                if (concordanceTestResults[row][column] >= lambda && discordanceTestResults[row][column] != 1.0) {
+                    result[row][column] = 1.0
+                } else {
+                    result[row][column] = 0.0
+                }
+            }
+        }
+
+        return result
     }
 }
