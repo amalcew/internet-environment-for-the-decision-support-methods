a9e5ddadaddaace9dda4389e44cb15c0c6943869
Klaudia <klaudia02460@gmail.com>
2023-09-07 16:01:57 +0200

    Add DatasetObject model and relations OneToMany with Criteria and Variants in Dataset model

--- src/main/kotlin/pl/poznan/put/decision_support/model/Dataset.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/model/Dataset.kt
@@ -6,11 +6,19 @@ import jakarta.persistence.*
 @Table(name = "datasets")
 data class Dataset(
     val name: String? = null
-){
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "dataset_id")
     var id: Long? = null
+
     @OneToOne
     @JoinColumn(name = "id")
     val project: Project? = null
+
+    @OneToMany(mappedBy = "dataset", cascade = [CascadeType.ALL])
+    val criteria: List<Criterion>? = null
+
+    @OneToMany(mappedBy = "dataset", cascade = [CascadeType.ALL])
+    val variants: List<Variant>? = null
 }
