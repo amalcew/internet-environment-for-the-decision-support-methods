a15fdb12c88a93b22103c5fbd414b39a2b5fd0ef
Klaudia <klaudia02460@gmail.com>
2023-09-07 16:05:12 +0200

    Add @EnableJpaRepositories annotation in Application class

--- src/main/kotlin/pl/poznan/put/decision_support/DecisionSupportWebBackendApplication.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/DecisionSupportWebBackendApplication.kt
@@ -2,8 +2,10 @@ package pl.poznan.put.decision_support
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories("pl.poznan.put.decision_support.repository")
 class DecisionSupportWebBackendApplication
 
 fun main(args: Array<String>) {
