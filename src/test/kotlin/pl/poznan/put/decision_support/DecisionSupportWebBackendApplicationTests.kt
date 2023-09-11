8bf55b4c225bbfabcc0bda64d94f5619e72d0368
Klaudia <klaudia02460@gmail.com>
2023-09-11 21:37:07 +0200

    Fix contextLoads test

--- src/test/kotlin/pl/poznan/put/decision_support/DecisionSupportWebBackendApplicationTests.kt
+++ src/test/kotlin/pl/poznan/put/decision_support/DecisionSupportWebBackendApplicationTests.kt
@@ -1,13 +1,27 @@
 package pl.poznan.put.decision_support
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import pl.poznan.put.decision_support.controller.DatasetController
+import pl.poznan.put.decision_support.service.dataset.DatasetService
+
 
 @SpringBootTest
 class DecisionSupportWebBackendApplicationTests {
 
+    @Autowired
+    private val datasetController: DatasetController? = null
+
+    @Autowired
+    private val datasetService: DatasetService? = null
+
     @Test
+    @Throws(Exception::class)
     fun contextLoads() {
+        assertThat(datasetController).isNotNull()
+        assertThat(datasetService).isNotNull()
     }
 
 }
