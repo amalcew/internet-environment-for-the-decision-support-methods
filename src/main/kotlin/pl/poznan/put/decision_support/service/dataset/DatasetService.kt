3b5fddd11e5372b26c5f0120307a3dad685310a2
Klaudia <klaudia02460@gmail.com>
2023-09-22 14:20:13 +0200

    Modify methods return type in DatasetService - add mapping from entity to domain model

--- src/main/kotlin/pl/poznan/put/decision_support/service/dataset/DatasetService.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/service/dataset/DatasetService.kt
@@ -2,12 +2,15 @@ package pl.poznan.put.decision_support.service.dataset
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import pl.poznan.put.decision_support.model.Criterion
-import pl.poznan.put.decision_support.model.Dataset
-import pl.poznan.put.decision_support.model.Variant
-import pl.poznan.put.decision_support.model.VariantCriterionValue
+import pl.poznan.put.decision_support.model.domain.Dataset
+import pl.poznan.put.decision_support.model.entity.CriterionEntity
+import pl.poznan.put.decision_support.model.entity.DatasetEntity
+import pl.poznan.put.decision_support.model.entity.VariantEntity
+import pl.poznan.put.decision_support.model.entity.VariantCriterionValueEntity
 import pl.poznan.put.decision_support.model.domain.DatasetObject
 import pl.poznan.put.decision_support.model.domain.toVariant
+import pl.poznan.put.decision_support.model.mapper.mapToDataset
+import pl.poznan.put.decision_support.model.mapper.toDatasetEntity
 import pl.poznan.put.decision_support.repository.CriterionRepository
 import pl.poznan.put.decision_support.repository.DatasetRepository
 import pl.poznan.put.decision_support.repository.VariantCriterionValueRepository
@@ -21,14 +24,14 @@ class DatasetService(
     private val criterionRepository: CriterionRepository,
     private val variantCriterionValueRepository: VariantCriterionValueRepository
 ) {
-    fun getAll(): List<Dataset?> = datasetRepository.findAll().toList()
+    fun getAll(): List<DatasetEntity?> = datasetRepository.findAll().toList()
 
-    fun getByName(name: String) = datasetRepository.getDatasetByName(name)
+    fun getByName(name: String) = datasetRepository.getDatasetByName(name)?.mapToDataset()
 
     @Transactional
-    fun save(dataset: Dataset) = datasetRepository.save(dataset)
+    fun save(dataset: Dataset) = datasetRepository.save(dataset.toDatasetEntity())
 
-    fun findById(id: Long): Optional<Dataset?> = datasetRepository.findById(id)
+    fun findById(id: Long): Optional<Dataset?> = datasetRepository.findById(id).map { it?.mapToDataset() }
 
     fun existsById(id: Long) = datasetRepository.existsById(id)
 
@@ -37,28 +40,30 @@ class DatasetService(
 
     @Transactional
     fun saveDatasetOfObjects(dataset: DatasetObject): Dataset {
-        var savedDataset = datasetRepository.save(Dataset(name = dataset.name))
-        val savedCriteria: List<Criterion> = dataset.criteria.map { criterion ->
-            criterionRepository.save(Criterion(name = criterion.name, dataset = savedDataset))
+        var savedDataset = datasetRepository.save(DatasetEntity(name = dataset.name))
+        val savedCriteria: List<CriterionEntity> = dataset.criteria.map { criterion ->
+            criterionRepository.save(CriterionEntity(name = criterion.name, dataset = savedDataset))
         }
-        val savedVariants = mutableListOf<Variant>()
+        val savedVariants = mutableListOf<VariantEntity>()
+        val values = mutableListOf<VariantCriterionValueEntity>()
         dataset.variants.forEach { variant ->
             val savedVariant = variantRepository.save(variant.toVariant().copy(dataset = savedDataset))
             savedVariants.add(savedVariant)
             savedCriteria.zip(variant.values).forEach { (criterion, value) ->
-                variantCriterionValueRepository.save(
-                    VariantCriterionValue(
-                        variant = savedVariant,
-                        criterion = criterion,
-                        value = value
+                values.add(
+                    variantCriterionValueRepository.save(
+                        VariantCriterionValueEntity(
+                            variant = savedVariant,
+                            criterion = criterion,
+                            value = value
+                        )
                     )
                 )
-
             }
         }
         savedDataset.criteria = savedCriteria
         savedDataset.variants = savedVariants
         savedDataset = datasetRepository.save(savedDataset)
-        return savedDataset
+        return savedDataset.mapToDataset(values)
     }
 }
