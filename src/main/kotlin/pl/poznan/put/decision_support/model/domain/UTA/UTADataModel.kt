d66427df10a85d3816ab4a1a43a37d3587768689
Klaudia <klaudia02460@gmail.com>
2023-09-22 17:30:10 +0200

    UTA intro - add data models, endpoints in controller

---
+++ src/main/kotlin/pl/poznan/put/decision_support/model/domain/UTA/UTADataModel.kt
@@ -0,0 +1,16 @@
+package pl.poznan.put.decision_support.model.domain.UTA
+
+import pl.poznan.put.decision_support.model.domain.Criterion
+import pl.poznan.put.decision_support.model.domain.Variant
+
+data class UTADataModel(
+    val criteria: List<Criterion>,
+    val variants: List<Variant>,
+    val referenceRanking: ReferenceRanking,
+    val maxLinearSections: List<LinearSection>
+)
+
+data class LinearSection(
+    val criterion: Criterion,
+    val maxLinearSection: Int = 1
+)
