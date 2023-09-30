dfdbf73851a21c683d293bc37944a5047d8c5755
Michał <michauz2001@gmail.com>
2023-09-30 11:29:11 +0200

    WIP - register endpoint

--- src/main/kotlin/pl/poznan/put/decision_support/users/controller/AuthController.kt
+++ src/main/kotlin/pl/poznan/put/decision_support/users/controller/AuthController.kt
@@ -6,19 +6,29 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import pl.poznan.put.decision_support.config.utils.JwtTokenFilter
 import pl.poznan.put.decision_support.config.utils.JwtTokenUtil
+import pl.poznan.put.decision_support.users.model.User
+import pl.poznan.put.decision_support.users.service.UserService
 
 
 @RestController
 @RequestMapping("/auth")
-class AuthController {
-    @Autowired
-    private val jwtTokenUtil: JwtTokenUtil? = null
+class AuthController @Autowired constructor(
+    private val jwtTokenUtil: JwtTokenUtil,
+    private val userService: UserService
+) {
     @PostMapping("/login")
     fun login(@RequestParam username: String?, @RequestParam password: String?): ResponseEntity<String> {
         // Validate username and password (e.g., against a user database)
         // If valid, generate a JWT token and return it
-        val token = jwtTokenUtil!!.generateToken(username)
+        val token = jwtTokenUtil.generateToken(username)
+        return ResponseEntity.ok(token)
+    }
+    @PostMapping("/register")
+    fun register(@RequestParam username: String?, @RequestParam password: String?): ResponseEntity<String> {
+        //todo create user in db
+        val token = jwtTokenUtil.generateToken(username)
         return ResponseEntity.ok(token)
     }
 }
