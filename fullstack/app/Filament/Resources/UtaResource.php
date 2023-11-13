28a2323d814d8af4a588270090081774275e8839
Klaudia <klaudia02460@gmail.com>
2023-11-13 20:11:05 +0100

    Add UTA resources and expand the model with additional attributes

---
+++ fullstack/app/Filament/Resources/UtaResource.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UtaResource\Pages;
+use App\Filament\Resources\UtaResource\RelationManagers;
+use App\Models\Uta;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UtaResource extends Resource
+{
+    protected static ?string $model = Uta::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUtas::route('/'),
+            'create' => Pages\CreateUta::route('/create'),
+            'edit' => Pages\EditUta::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ fullstack/app/Filament/Resources/UtaResource/Pages/CreateUta.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\UtaResource\Pages;
+
+use App\Filament\Resources\UtaResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateUta extends CreateRecord
+{
+    protected static string $resource = UtaResource::class;
+}
