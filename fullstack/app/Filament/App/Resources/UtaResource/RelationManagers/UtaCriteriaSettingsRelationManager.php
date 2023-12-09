2cf87d9cb736da586e86f7b5bb9214b704b2d428
Klaudia <klaudia02460@gmail.com>
2023-12-09 14:38:26 +0100

    Uta criteria settings filters and table columns improvements

--- fullstack/app/Filament/App/Resources/UtaResource/RelationManagers/UtaCriteriaSettingsRelationManager.php
+++ fullstack/app/Filament/App/Resources/UtaResource/RelationManagers/UtaCriteriaSettingsRelationManager.php
@@ -7,6 +7,7 @@ use Filament\Facades\Filament;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
 use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Filament\Forms;
 use Filament\Tables;
@@ -48,12 +49,27 @@ class UtaCriteriaSettingsRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('id')
             ->columns([
-                Tables\Columns\TextColumn::make('criterion.name'),
-                Tables\Columns\TextColumn::make('type'),
+                Tables\Columns\TextColumn::make('criterion.name')->searchable()->sortable(),
+                Tables\Columns\TextColumn::make('type')
+                    ->badge()
+                    ->color(fn (string $state): string => match ($state) {
+                        'cost' => 'danger',
+                        'gain' => 'success',
+                    }),
                 Tables\Columns\TextColumn::make('linear_segments'),
             ])
             ->filters([
-                //
+                SelectFilter::make('criterion')
+                    ->relationship('criterion', 'name')
+                    ->searchable()
+                    ->preload(),
+                SelectFilter::make('type')
+                    ->options([
+                        'cost' => 'cost',
+                        'gain' => 'gain',
+                    ])
+                    ->searchable()
+                    ->preload()
             ])
             ->headerActions([
                 Tables\Actions\CreateAction::make(),
@@ -68,7 +84,9 @@ class UtaCriteriaSettingsRelationManager extends RelationManager
                 ]),
             ])
             ->emptyStateActions([
-                Tables\Actions\CreateAction::make(),
-            ]);
+                Tables\Actions\CreateAction::make()->label('Add criteria setting'),
+            ])
+            ->emptyStateHeading('You haven\'t added any criteria setting yet. Let\'s start!')
+            ->emptyStateDescription('Criteria settings are used to define the type of criteria and the number of linear segments.');
     }
 }
