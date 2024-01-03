c614e893922b1bf6d8e142db98031b7e53e13476
KlaudiaK <klaudia02460@gmail.com>
2024-01-03 21:18:41 +0100

    Remove unused classes from domain folder

--- backend/src/main/kotlin/pl/poznan/put/decision_support/decisionmethods/model/domain/Criterion.kt
+++
@@ -1,7 +0,0 @@
-package pl.poznan.put.decision_support.decisionmethods.model.domain
-
-data class Criterion(
-    var id: Long? = null,
-    val name: String? = null,
-    val type: CriterionType? = null
-)
--- backend/src/main/kotlin/pl/poznan/put/decision_support/decisionmethods/model/domain/UTA/PreorderRelationType.kt
+++
@@ -1,8 +0,0 @@
-package pl.poznan.put.decision_support.decisionmethods.model.domain.UTA
-
-/**
- * Typ relacji pomiędzy wariantami: preferencja i nierozróżnialność
- */
-enum class PreorderRelationType {
-    PREFERENCE, INDISTINGUISHABILITY
-}
